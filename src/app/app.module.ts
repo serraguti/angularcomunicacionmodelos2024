d6aef08eeab8a5a377ed4154985e502357bb1d9f
serraguti <pacoserranox@gmail.com>
2024-11-07 14:29:08 +0100

    cczx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductosComponent
   ],
   imports: [
     BrowserModule,
